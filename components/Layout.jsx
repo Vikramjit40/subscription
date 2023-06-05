f5ee19140b3e3c13249f92bd2ec61e303c80639f
Vikramjit40 <vikramjit@sookshum-labs.com>
2023-06-05 18:31:37 +0530

    commitsnav

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({children}) => {
   return (
     <div className='h-screen flex flex-row justify-start '> 
       <Sidebar />
-      <div className=' bg-backcolor flex-1 pt-4 '>
+      <div className=' bg-backcolor flex-1 pt-4 'id="body">
         {children}
         <Bottombar />
       </div>
